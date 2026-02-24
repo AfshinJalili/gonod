77fd1ef2cd0564e8bbcbe4b466c220582e652248
Afshin Jalili <afshnjalili@gmail.com>
2026-02-24 15:21:13 +0330

    Add basic HTTP server with health check endpoint

--- cmd/server.go
+++ cmd/server.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		if err := json.NewEncoder(w).Encode(struct {
+			Status string `json:"status"`
+		}{Status: "ok"}); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+}
